16139df54abe5e118737b07e9379a7b0a30bb50d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-31 16:16:34 +0700

    membuat variabel untuk garis dibawah kotak alamat

--- app/src/main/java/com/example/questuserinput_027/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_027/TeksEdit.kt
@@ -84,4 +84,11 @@ fun FormDataDiri(
                 textAlamat = it
             }
         )
-}
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium),
+                top = dimensionResource(id = R.dimen.padding_medium)
+            ),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
