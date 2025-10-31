d4241e274f61b730ee74a5564231f7044af6f67c
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-31 16:13:36 +0700

    membuat variabel untuk lanjutan gender tadi yang vertical dan menggunakan radiopbutton

--- app/src/main/java/com/example/questuserinput_027/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_027/TeksEdit.kt
@@ -64,4 +64,14 @@ fun FormDataDiri(
                 Row(modifier = Modifier.selectable(
                     selected = textJK == item,
                     onClick = {textJK = item}
-                ),}
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(text = item)
+                }
+            }
+        }
+}
